fe5b4ab2bd909c13b3b1f5a559b6b480771b1116
Juan Sebastian Morel Ruiz Diaz <juanse1990@live.com.ar>
2024-10-14 15:08:07 -0300

    Feature/Eliminar Noticia, Creacion perfiles

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -1,18 +1,19 @@
-
 from django.urls import path
 from . import views
 
 app_name = 'noticias'
 
 urlpatterns = [
-	
+
 	path('', views.Listar_Noticias, name = 'listar'),
 
 	path('Crear/', views.Crear_Noticia, name = 'crear_noticia'),
 
+	path('Eliminar/<int:pk>/', views.Eliminar_Noticia, name='eliminar'),
+
 	path('Detalle/<int:pk>', views.Detalle_Noticias, name = 'detalle'),
-	
+
 	path('Comentario/Agregar', views.Comentar_Noticia, name = 'comentar'),
-	
+
 	path('Comentario/Eliminar/<int:pk>', views.Eliminar_Comentario, name= 'eliminar_comentario'),
 ]
