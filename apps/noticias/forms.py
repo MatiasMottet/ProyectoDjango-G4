fbfabaef3e082329f4cd2edd973e1c444a3a24a7
Matías Mottet <msmottet@gmail.com>
2024-10-19 16:26:44 -0300

    Se agrega categorias y contraste a la card

--- apps/noticias/forms.py
+++ apps/noticias/forms.py
@@ -1,10 +1,17 @@
 from django import forms
-from .models import Noticia, Denuncia
+from .models import Noticia, Categoria, Denuncia
 
 class NoticiaForm(forms.ModelForm):
+    categoria_noticia = forms.ModelChoiceField(
+        queryset=Categoria.objects.all(),
+        empty_label="Selecciona una categoría",
+        required=True
+    )
+
     class Meta:
         model = Noticia
-        fields = ['titulo', 'cuerpo', 'imagen','categoria_noticia']
+        fields = ['titulo', 'cuerpo', 'imagen', 'categoria_noticia']
+
 
 class DenunciaForm(forms.ModelForm):
     class Meta:
